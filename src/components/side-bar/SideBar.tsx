4753a0232f8290d6604940963977f80214b0a10a
Joselu <joseluissgz97@gmail.com>
2025-02-13 22:41:04 +0100

    feat: refactor SideBar to use sections and integrate DefaultSections component

--- src/components/side-bar/SideBar.tsx
+++ src/components/side-bar/SideBar.tsx
@@ -1,48 +1,37 @@
 import { closestCenter, DndContext, DragEndEvent } from '@dnd-kit/core';
 import { arrayMove, SortableContext, verticalListSortingStrategy } from '@dnd-kit/sortable';
-import { useState } from 'react';
-import { People } from '../../interfaces/people.interface';
-import { User } from '../User';
+import { useSection } from '../../hooks/useSection';
+import { DefaultSections } from '../default-sections/DefaultSections';
+import { SectionItem } from '../SectionItem';
 import './sideBar.css';
 
 export const SideBar = () => {
-  const [people, setPeople] = useState<People[]>([
-    {
-      id: 1,
-      name: 'John',
-    },
-    {
-      id: 2,
-      name: 'Paco',
-    },
-    {
-      id: 3,
-      name: 'Joselu',
-    },
-    {
-      id: 4,
-      name: 'Sara',
-    },
-  ]);
+  const { sections, newSectionSort } = useSection();
 
   const handleDragEnd = (event: DragEndEvent) => {
     const { active, over } = event;
-    const previusIndex = people.findIndex((person) => person.id === active.id);
-    const newIndex = people.findIndex((person) => person.id === over?.id);
-    const newArray = arrayMove(people, previusIndex, newIndex);
-    setPeople(newArray);
+    const previusIndex = sections.findIndex((person) => person.id === active.id);
+    const newIndex = sections.findIndex((person) => person.id === over?.id);
+    const newArray = arrayMove(sections, previusIndex, newIndex);
+    newSectionSort(newArray);
   };
   return (
     <aside className="sidebar">
       <DndContext collisionDetection={closestCenter} onDragEnd={handleDragEnd}>
         <h2>Your sections</h2>
-        <SortableContext items={people} strategy={verticalListSortingStrategy}>
-          <div className="sections">
-            {people.map((person) => (
-              <User key={person.id} people={person} />
+        <SortableContext items={sections} strategy={verticalListSortingStrategy}>
+          <ul className="sections">
+            {sections.map((section) => (
+              <SectionItem key={section.id} section={section} />
             ))}
-          </div>
+          </ul>
         </SortableContext>
+
+        <section>
+          <h2> Sections Avalilable</h2>
+
+          <DefaultSections />
+        </section>
       </DndContext>
     </aside>
   );

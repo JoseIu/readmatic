73c699de314d07b15c62b9b22041315a4fe96039
Joselu <joseluissgz97@gmail.com>
2025-02-13 22:40:40 +0100

    feat: integrate section content into MonacoEditor

--- src/components/MonacoEditor/MonacoEditor.tsx
+++ src/components/MonacoEditor/MonacoEditor.tsx
@@ -1,8 +1,10 @@
 import { Editor, type Monaco } from '@monaco-editor/react';
+import { useSection } from '../../hooks/useSection';
 import OneDarkPro from '../../theme/onedarkpro.json';
 import './monacoEditor.css';
 
 export const MonacoEditor = () => {
+  const { sectionActive } = useSection();
   const handleEditorDidMount = (monaco: Monaco) => {
     monaco.editor.defineTheme('OneDarkPro', {
       base: 'vs-dark',
@@ -13,10 +15,11 @@ export const MonacoEditor = () => {
   return (
     <div className="editor">
       <Editor
-        height="100dvh"
+        height="calc(100vh - 3.1rem)"
         width="100%"
         theme="OneDarkPro"
         options={{ minimap: { enabled: false } }}
+        value={sectionActive?.content ?? ''}
         defaultLanguage="markdown"
         beforeMount={handleEditorDidMount}
       />

2c95fdae97bd15a30a41b79fe9546d09dc3fb122
Joselu <joseluissgz97@gmail.com>
2025-02-17 12:06:55 +0100

    feat:  add onUpdadeSection

--- src/components/MonacoEditor/MonacoEditor.tsx
+++ src/components/MonacoEditor/MonacoEditor.tsx
@@ -4,13 +4,19 @@ import OneDarkPro from '../../theme/onedarkpro.json';
 import './monacoEditor.css';
 
 export const MonacoEditor = () => {
-  const { sectionActive } = useSection();
+  const { sectionActive, updateSection } = useSection();
   const handleEditorDidMount = (monaco: Monaco) => {
     monaco.editor.defineTheme('OneDarkPro', {
       base: 'vs-dark',
       inherit: true,
       ...OneDarkPro,
     });
+  };
+
+  const onUpdadeSection = (value: string | undefined) => {
+    if (!value) return;
+    if (!sectionActive) return;
+    updateSection(sectionActive.id, value);
   };
   return (
     <div className="editor">
@@ -22,6 +28,7 @@ export const MonacoEditor = () => {
         value={sectionActive?.content ?? ''}
         defaultLanguage="markdown"
         beforeMount={handleEditorDidMount}
+        onChange={(value) => onUpdadeSection(value)}
       />
     </div>
   );

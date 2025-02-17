08e6665907ea416c5bb9624da5ca5aabc05997a5
Joselu <joseluissgz97@gmail.com>
2025-02-17 12:08:18 +0100

    feat: add sectionStore to manege local storage

---
+++ src/utils/sectionStore.ts
@@ -0,0 +1,22 @@
+import { Section } from '../interfaces/section.interface';
+const SECTION_KEY = 'sections';
+export const getSectionsFromStorage = (): Section[] => {
+  const sections = JSON.parse(localStorage.getItem(SECTION_KEY) || '[]') as Section[];
+
+  return sections;
+};
+
+export const saveSectionsToStorage = (sections: Section[]) => {
+  localStorage.setItem(SECTION_KEY, JSON.stringify(sections));
+};
+
+export const updateSectionInStorage = (sectionId: string, content: string) => {
+  const sections = getSectionsFromStorage();
+  const sectionToUpdate = sections.findIndex((section) => section.id === sectionId);
+
+  if (sectionToUpdate === -1) return;
+
+  sections[sectionToUpdate].content = content;
+
+  saveSectionsToStorage(sections);
+};

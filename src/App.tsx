faed0ad3a4cffe7e9f33e3bb9b42f55aa18176e8
Joselu <joseluissgz97@gmail.com>
2025-02-18 17:06:01 +0100

    feat: replace div for fragment and replace div for main for semantic HTML

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,29 @@
+import { Toaster } from 'react-hot-toast';
 import './App.css';
 import { Header } from './components/header/Header';
 import { MonacoEditor } from './components/MonacoEditor/MonacoEditor';
-import { Preview } from './components/preview/Preview';
+import { PreviewSection } from './components/preview-section/PreviewSection';
 import { SideBar } from './components/side-bar/SideBar';
 import { SectionProvider } from './context/SectionProvider';
 
 function App() {
   return (
-    <div>
+    <>
       <Header />
 
-      <div className="content">
+      <main className="content">
         <SectionProvider>
           <SideBar />
 
           <div className="content__editor">
             <MonacoEditor />
-            <Preview />
+
+            <PreviewSection />
           </div>
         </SectionProvider>
-      </div>
-    </div>
+      </main>
+      <Toaster position="top-right" toastOptions={{ duration: 2500 }} />
+    </>
   );
 }
 

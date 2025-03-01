52c6dfadb46c1bd5f20facb1edd837915f9e83e0
Joselu <joseluissgz97@gmail.com>
2025-03-01 19:40:14 +0100

    Feat: add SearchSectionByTerm

--- src/components/default-sections/DefaultSections.tsx
+++ src/components/default-sections/DefaultSections.tsx
@@ -1,13 +1,31 @@
+import { useState } from 'react';
 import { useSection } from '../../hooks/useSection';
+import { Section } from '../../interfaces/section.interface';
+import { Search } from '../search/Search';
 import { DefaultSectionItem } from './DefaultSectionItem';
 
 export const DefaultSections = () => {
   const { defaultSections } = useSection();
+
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const defaultSectionsFiltered = SearchSectionByTerm(defaultSections, searchTerm);
   return (
-    <ul className="sections">
-      {defaultSections.map((section) => (
-        <DefaultSectionItem key={section.id} section={section} />
-      ))}
-    </ul>
+    <>
+      <Search setSearchTerm={setSearchTerm} />
+      <ul className="sections">
+        {defaultSectionsFiltered.map((section) => (
+          <DefaultSectionItem key={section.id} section={section} />
+        ))}
+      </ul>
+    </>
   );
+};
+
+const SearchSectionByTerm = (sections: Section[], searchTerm: string) => {
+  if (!searchTerm) return sections;
+
+  const searchTermLower = searchTerm.toLowerCase();
+
+  return sections.filter((section) => section.title.toLowerCase().includes(searchTermLower));
 };
